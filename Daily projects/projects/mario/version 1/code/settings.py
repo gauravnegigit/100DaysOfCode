5a7c5e345fa732e5742ca15f271f67754a7c2d23
Gaurav Negi <97227844+gauravnegigit@users.noreply.github.com>
2022-05-22 21:05:37 +0530

    Add files via upload

---
+++ Daily projects/projects/mario/version 1/code/settings.py
@@ -0,0 +1,18 @@
+level_map = [
+    '                            ' , 
+    '                            ' , 
+    ' P                          ' , 
+    ' XX    XXXX          XX     ' , 
+    ' XX                         ' , 
+    ' XXXX            XX      XX ' , 
+    ' XXXX        XX             ' , 
+    'XX      X  XXXXXXXX    XX   ' , 
+    '        X XXXX     XX   XXX ' , 
+    '    XXXX  XXXXXX  XX  XXXX  ' , 
+    'XXXXXXXX  XXXXXX  XX  XXXX  ' , 
+]
+
+tile_size = 64 
+
+# screen constants 
+WIDTH , HEIGHT = 1200 , len(level_map) * 64 
